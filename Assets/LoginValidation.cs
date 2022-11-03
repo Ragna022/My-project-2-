8131554ac22cb14e2cb53ebe5ade06e10cd50356
Ragna022 <gideonite4christ2017@gmail.com>
2022-11-03 08:49:19 +0100

    New Task

---
+++ Assets/LoginValidation.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoginValidation : MonoBehaviour
+{
+
+    public TMP_InputField USERNAME;
+    public TMP_InputField PASSWORD;
+
+    public GameObject ScreenPanel;
+
+
+    public void CheckValidation()
+    {
+        string uname = USERNAME.text;
+        string pass = PASSWORD.text;
+
+        if (uname == "Pezi" && pass == "PeziTask")
+        {
+            Debug.Log("Login Successfully....");
+            ScreenPanel.SetActive(false);
+        }
+        else if (uname == "" || pass == "")
+        {
+            Debug.Log("Please Enter Username And Password...");
+            ScreenPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Please Enter Correct Username And Password...");
+            ScreenPanel.SetActive(true);
+        }
+    }
+
+    
+   
+}
